8dd503e5c1d4c2ea820434a6f90c3bd2679242dc
asilyN <naene30gust@gmail.com>
2024-09-03 15:21:39 +0800

    updated code

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 const form = document.querySelector('form');
-        const taskList = document.querySelector('#task-list');
+const taskList = document.querySelector('#task-list');
 
-        function addTask(task) {
+    function addTask(task) {
+        if(task === '') {
+            alert('Please enter a task');
+        }
+        else{
             const listItem = document.createElement('li');
-            const uniqueId = `task-${Date.now()}`; // Generate a unique ID for each task
-        
+            const uniqueId = `task-${Date.now()}`; 
             listItem.innerHTML = `
                 <input type="checkbox" id="${uniqueId}" />
                 <label for="${uniqueId}">
-                    <img src="unchecked.png" alt="Unchecked" class="checkbox-image" />
+                    <img src="unchecked.png" class="checkbox-image" />
                 </label>
                 <span>${task}</span>
                 <button class ="delete-btn">Delete</button>
             `;
             taskList.appendChild(listItem);
+    }  
+    
+}
+    form.addEventListener('submit', (e) => {
+        event.preventDefault();
+        const input = document.querySelector('#task-input');
+        const task = input.value;
+        addTask(task);
+        input.value = '';
+    });
+
+    taskList.addEventListener('click', (event) => {
+        if(event.target.tagName === 'BUTTON') {
+            const listIteam = event.target.parentElement;
+            taskList.removeChild(listIteam);
         }
+    });
 
-        form.addEventListener('submit', (e) => {
-            event.preventDefault();
-            const input = document.querySelector('#task-input');
-            const task = input.value;
-            addTask(task);
-            input.value = '';
-        });
-
-        taskList.addEventListener('click', (event) => {
-            if(event.target.tagName === 'BUTTON') {
-                const listIteam = event.target.parentElement;
-                taskList.removeChild(listIteam);
-            }
-        });
+    function saveData(){
+        localStorage.setItem("uniqueId", taskList.innerHTML);
+    }
+    function showTask(){
+        taskList.innerHTML = localStorage.getItem("uniqueId");
+    }
+    showTask();
